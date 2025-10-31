7429a7a022849dba747aca0a3ba31bdeaa936299
Simar25 <simaranand250@gmail.com>
2025-10-31 21:19:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>DNS Simulator is Working 🚀</h1>
+    </div>
+  );
+}
 
